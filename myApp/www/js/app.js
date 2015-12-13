a55cf0a0b2b8e46763d2e489573c4e45fca1e522
Edward Gilmore <edgilmore@live.com>
2015-12-13 12:22:57 -0700

    adding pages

---
+++ myApp/www/js/app.js
@@ -0,0 +1,136 @@
+// Ionic template App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'SimpleRESTIonic' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+angular.module('SimpleRESTIonic', ['ionic', 'backand', 'SimpleRESTIonic.controllers', 'SimpleRESTIonic.services'])
+
+    .run(function ($ionicPlatform) {
+        $ionicPlatform.ready(function () {
+            // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+            // for form inputs)
+            if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+                cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+                cordova.plugins.Keyboard.disableScroll(true);
+
+            }
+            if (window.StatusBar) {
+                // org.apache.cordova.statusbar required
+                StatusBar.styleLightContent();
+            }
+        });
+    })
+    .factory('audio',function ($document) {
+      var audioElement = $document[0].createElement('audio'); // <-- Magic trick here
+      return {
+        audioElement: audioElement,
+
+        play: function(filename) {
+          audioElement.src = filename;
+          audioElement.play();     //  <-- Thats all you need
+        },
+        pause: function() {
+          audioElement.pause();
+        }
+        // Exersise for the reader - extend this service to include other functions
+        // like pausing, etc, etc.
+
+      }
+    })
+    .config(function (BackandProvider, $stateProvider, $urlRouterProvider, $httpProvider) {
+	      BackandProvider.runSocket(true);
+        BackandProvider.setAppName('animatedoctocouscous'); // change here to your app name
+        BackandProvider.setSignUpToken('9f12b8da-0882-4347-a6bf-72728634295f'); //token that enable sign up. see http://docs.backand.com/en/latest/apidocs/security/index.html#sign-up
+        BackandProvider.setAnonymousToken('aa863ba0-7be9-4b73-8862-6bb69d10a113'); // token is for anonymous login. see http://docs.backand.com/en/latest/apidocs/security/index.html#anonymous-access
+
+        $stateProvider
+            // setup an abstract state for the tabs directive
+            .state('main', {
+                url: '/main',
+                abstract: true,
+                templateUrl: 'templates/main.html'
+            })
+            .state('main.home', {
+                url: '/home',
+                views: {
+                    'main-home': {
+                        templateUrl: 'templates/main-home.html',
+                        controller: 'HomeCtrl as homeVM'
+                    }
+                }
+            })
+            .state('main.create', {
+                url: '/create',
+                views: {
+                    'main-create': {
+                        templateUrl: 'templates/main-create.html',
+                        controller: 'CreateCtrl as createVM'
+                    }
+                }
+            })
+            .state('main.join', {
+              url: '/join',
+              views: {
+                'main-join': {
+                  templateUrl: 'templates/main-join.html',
+                  controller: 'JoinCtrl as joinVM'
+                }
+              },
+              resolve: {
+                sessions: function(SessionService) {
+                  return SessionService.all();
+                }
+              }
+            })
+          .state('conductor', {
+            url: '/conductor/:id',
+            templateUrl: 'templates/conductor.html',
+            controller: 'ConductorCtrl as conductorVM',
+            resolve: {
+              session: function(SessionService, $stateParams) {
+                return SessionService.fetch($stateParams.id);
+              }
+            }
+          })
+          .state('choir', {
+            url: '/choir/:id',
+            templateUrl: 'templates/choir.html',
+            controller: 'ChoirCtrl as choirVM',
+            resolve: {
+              session: function(SessionService, $stateParams) {
+                return SessionService.fetch($stateParams.id);
+              }
+            }
+          });
+
+        $urlRouterProvider.otherwise('/main/home');
+
+        //$httpProvider.interceptors.push('APIInterceptor');
+    })
+
+    //.run(function ($rootScope, $state, LoginService, Backand) {
+    //
+    //    function unauthorized() {
+    //        console.log("user is unauthorized, sending to login");
+    //        $state.go('tab.login');
+    //    }
+    //
+    //    function signout() {
+    //        LoginService.signout();
+    //    }
+    //
+    //    $rootScope.$on('unauthorized', function () {
+    //        unauthorized();
+    //    });
+    //
+    //    $rootScope.$on('$stateChangeSuccess', function (event, toState) {
+    //        if (toState.name == 'tab.login') {
+    //            signout();
+    //        }
+    //        else if (toState.name != 'tab.login' && Backand.getToken() === undefined) {
+    //            unauthorized();
+    //        }
+    //    });
+    //
+    //})
+
---
+++ myApp/www/js/main.controller.js
@@ -0,0 +1,127 @@
+/**
+ * Created by vageeshb on 12/12/15.
+ */
+'use strict';
+angular.module('SimpleRESTIonic.controllers', [])
+
+  .controller('HomeCtrl', function ($rootScope) {
+  })
+  .controller('CreateCtrl', function (SessionService, $rootScope, $location) {
+    var vm = this;
+
+    vm.create = function (object) {
+      SessionService.create(object)
+        .then(function (data) {
+          var sessionId = data.data.__metadata.id;
+          redirect(sessionId);
+        });
+    }
+    function redirect(id) {
+      $location.path('/conductor/' + id);
+    }
+    vm.cancelCreate = function() {
+      $location.path('/main/home');
+    };
+  })
+  .controller('JoinCtrl', function ($rootScope, sessions) {
+    var vm = this;
+    vm.sessions = sessions.data.data;
+  })
+  .controller('ConductorCtrl', function ($rootScope, session, SessionService) {
+    var vm = this;
+    vm.session = session;
+
+    vm.colors = [
+      { id: 1, name: 'Red' },
+      { id: 2, name: 'White' },
+      { id: 3, name: 'Green' }
+    ];
+
+    vm.songs = [
+      { url: 'http://www.freexmasmp3.com/files/jingle-bells-country.mp3', title: 'Jingle Bells Country' },
+      { url: 'http://www.freexmasmp3.com/files/silent-night-disco.mp3', title: 'Silent Night Disco' }
+    ];
+
+    vm.update = function() {
+      session.data.color = vm.color;
+      SessionService.update(session.data.id, session.data)
+        .then(function(res) {
+          //console.log(res)
+        });
+    };
+
+    vm.play = function() {
+      vm.songs.forEach(function(song) {
+        if(song.url === vm.song) {
+          song.isPlaying = true;
+          updateSession(song);
+        }
+      });
+    };
+
+    vm.pause = function() {
+      vm.songs.forEach(function(song) {
+        if(song.url === vm.song) {
+          song.isPlaying = false;
+          updateSession(song);
+        }
+      });
+    };
+
+    function updateSession(song) {
+      session.data.url = song.url;
+      session.data.title = song.title;
+      session.data.isPlaying = song.isPlaying;
+      SessionService.update(session.data.id, session.data)
+        .then(function(res) {
+          //console.log(res)
+        });
+    }
+
+  })
+  .controller('ChoirCtrl', function ($rootScope, session, $location, Backand, audio) {
+    var vm = this;
+    var context = new AudioContext();
+
+    vm.goHome = function(){
+      $location.path('/main/home');
+    };
+    Backand.on('color_updated', function(data){
+      console.log(data)
+        if (data[0].Value === session.data.id) {
+          vm.className = data[3].Value.toLowerCase();
+          if(data[4].Value) {
+            audio.play(data[5].Value);
+          } else if(!data[4].Value) {
+            audio.pause();
+          }
+        }
+
+    });
+    vm.session = session;
+
+    vm.url = 'http://www.freexmasmp3.com/files/silent-night-disco.mp3';
+
+    //load('http://www.freexmasmp3.com/files/silent-night-disco.mp3');
+    //play(vm.buffer);
+
+    function load(url) {
+      var request = new XMLHttpRequest();
+      request.open('GET', url, true);
+      request.responseType = 'arraybuffer';
+
+      request.onload = function() {
+        context.decodeAudioData(request.response, function(buffer) {
+          vm.buffer = buffer;
+        }, onError);
+      }
+      request.send();
+    }
+
+    function play(buffer) {
+      var source = context.createBufferSource();
+      source.buffer = buffer;
+      source.connect(context.destination);
+      source.start(0);
+    }
+  });
